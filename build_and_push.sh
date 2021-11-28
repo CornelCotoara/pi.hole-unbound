c21561381a4b37896bab574c5c9394de9e355675
CornelCotoara <94889846+CornelCotoara@users.noreply.github.com>
2021-11-28 09:49:11 +0200

    Not needed here!

--- build_and_push.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Run this once: docker buildx create --use --name build --node build --driver-opt network=host
-docker buildx build --platform linux/arm/v7,linux/arm64/v8,linux/amd64 -t cbcrowe/pihole-unbound:`cat VERSION` --push .
-docker buildx build --platform linux/arm/v7,linux/arm64/v8,linux/amd64 -t cbcrowe/pihole-unbound:latest --push .
-
-
